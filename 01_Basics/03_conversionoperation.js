f976dca64f4854249d9e7134f17b509042e1e6f9
Shahil Tiwari <shahiltwari011@gmail.com>
2026-03-30 19:39:39 +0530

    your message

---
+++ 01_Basics/03_conversionoperation.js
@@ -0,0 +1,39 @@
+let score = "66abs"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let num = Number(score)
+
+console.log(typeof num);
+console.log(num);
+
+/*
+
+"66" => 66
+
+"66abs" => NaN
+
+true => 1 
+false => 0
+
+"" => false
+
+"value" => true
+
+
+*/
+
+let loggedIn = 1
+
+let booleanLoggedIn = Boolean(loggedIn)
+
+console.log(booleanLoggedIn)
+
+let sommenumber = 55
+
+let stringnumber = String(sommenumber)
+
+console.log(stringnumber)
+
+console.log(typeof stringnumber)
